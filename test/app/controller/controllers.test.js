190d2283a5044b9d55781e0883130d230cb6db7f
shakilhasan <shakilhasan105268@gmail.com>
2022-01-11 11:51:26 +0600

    undo contact test

--- test/app/controller/controllers.test.js
+++
@@ -1,6 +0,0 @@
-const { healthHandler } = require('../../../src/controllers/health-controller')
-test('Health monitor controller', () => {
-  const res = { json: jest.fn(), sendStatus: jest.fn() }
-  healthHandler({}, res)
-  expect(res.json.mock.calls[0][0]).toEqual({ message: "OK" })
-})
