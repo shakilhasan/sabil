2428f0be81381ee7c4dda776155394adb929b3e2
Md.Shakil Hasan Ibn Younus <shakilhasan105268@gmail.com>
2022-08-13 14:10:58 +0600

    like button ui functionality and api

--- server/src/modules/blog/controller.js
+++ server/src/modules/blog/controller.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 const express = require("express");
+const mongoose = require("mongoose");
+const eventEmitter = require("nodemon");
 const { getQuery } = require("./service");
 const {
   getByIdHandler,
@@ -11,16 +13,13 @@ const {
 } = require("../../core/controller");
 const { validate } = require("./request");
 const { handleValidation } = require("../../common/middlewares");
-const mongoose = require("mongoose");
 
 const router = express.Router();
 
 const searchHandler = async (req, res, next) => {
   console.log("blog searchHandler body---------- ", req.body);
-  const user = await mongoose.models.User.findOne(
-      { _id: req.user.id  },
-  );
-  req.searchQuery = getQuery(req.body,user.followings);
+  const user = await mongoose.models.User.findOne({ _id: req.user.id });
+  req.searchQuery = getQuery(req.body, user.followings);
   return baseSearchHandler(req, res, next);
 };
 
@@ -28,7 +27,22 @@ const countHandler = async (req, res, next) => {
   req.searchQuery = getQuery(req.body);
   return baseCountHandler(req, res, next);
 };
+const updateLikeHandler = async (req,next) => {
+  const { body } = req;
+  const favorite = parseInt(body.favorite, 10) + 1;
+  try {
+    const doc = await mongoose.models[req.modelName].findOneAndUpdate(
+      { _id: body.id },
+      { $set: { favorite } }
+    );
+    console.log(req.user.id, "-------------", doc.favorite);
+    eventEmitter.emit(`${req.modelName}Updated follow`);
+    return doc.favorite;
+  } catch (e) {
+    return next(error, req, res);
 
+  }
+};
 router.get("/detail", getByIdHandler);
 router.post(
   "/create",
@@ -40,7 +54,7 @@ router.put(
   // handleValidation(validate),
   updateHandler
 );
-
+router.put("/updateLike", updateLikeHandler);
 router.post("/search", searchHandler);
 router.post("/count", countHandler);
 router.delete("/delete", deleteHandler);
