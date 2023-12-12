67e0ba82356538841b73e5aaa4f2b601b677a33c
Shakil Hasan Bin Younus <shakilhasan105268@gmail.com>
2023-12-12 18:25:32 +0600

    *WIP ts

--- server/src/email/sendgrid.ts
+++ server/src/email/sendgrid.ts
@@ -3,8 +3,8 @@ import config from "config";
 const SENDGRID_API_KEY = config.get<string>("SENDGRID_API_KEY");
 sgMail.setApiKey(SENDGRID_API_KEY);
 const msg = {
-    to: "foyzulkarim@gmail.com", // Change to your recipient
-    from: "info@bizbook365.com", // Change to your verified sender
+    to: "", // Change to your recipient
+    from: "", // Change to your verified sender
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
